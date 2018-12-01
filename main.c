66d4762f846df155339afc7f516dc071c4adb428
Cao Tan <tanpro260196@gmail.com>
2018-11-30 22:09:04 -0800

    Update main.c

--- main.c
+++ main.c
@@ -51,8 +51,8 @@ void second(int Input);
 void third(int input);
 int main(void)
 {
-    ui8Adjust = 100;
     //PWM variables
+    ui8Adjust = 100;
     //System Clock 40MHZ
     SysCtlClockSet(
             SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN
@@ -72,13 +72,13 @@ int main(void)
     //Specify pin D0 to the PWM_0, module 1.
     GPIOPinConfigure(GPIO_PD0_M1PWM0);
 
-    //Enable All Remaining GPIO
+    //Enable All Remaining GPIO port
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA); //peripheral A
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB); //peripheral B
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE); //peripheral E
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC); //peripheral C
 
-    //Since we don't know what pin we will use.
+    //Set all the pin that the 7-segments will use to Output
     GPIOPinTypeGPIOOutput(
             GPIO_PORTA_BASE,
             GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6
@@ -95,7 +95,7 @@ int main(void)
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE,
                           GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
 
-    //Unlock the switches. Locked by default.
+    //Unlock the switches. Locked by default. We nolonger use switch. So it may not needed but I don't have time to test anymore.
     HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY;
     HWREG(GPIO_PORTF_BASE + GPIO_O_CR) |= 0x01;
     HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = 0;
@@ -118,23 +118,22 @@ int main(void)
     PWMOutputState(PWM1_BASE, PWM_OUT_0_BIT, true);
     //Start PWM generator 0, module 1.
     PWMGenEnable(PWM1_BASE, PWM_GEN_0);
-//ADC0 Config
+    
+    //ADC0 Config
     /* enable clocks */
     SYSCTL_RCGCGPIO_R |= 0x10; /* enable clock to GPIOE (AIN0 is on PE3) */
     SYSCTL_RCGCADC_R |= 1; /* enable clock to ADC0 */
-
     /* initialize PE3 for AIN0 input  */
     GPIO_PORTE_AFSEL_R |= 8; /* enable alternate function */
     GPIO_PORTE_DEN_R &= ~8; /* disable digital function */
     GPIO_PORTE_AMSEL_R |= 8; /* enable analog function */
-
     /* initialize ADC0 */
     ADC0_ACTSS_R &= ~8; /* disable SS3 during configuration */
     ADC0_EMUX_R &= ~0xF000; /* software trigger conversion */
     ADC0_SSMUX3_R = 0; /* get input from channel 0 */
     ADC0_SSCTL3_R |= 6; /* take one sample at a time, set flag at 1st sample */
     ADC0_ACTSS_R |= 8; /* enable ADC0 sequencer 3 */
-//End ADC0 Config
+    //End ADC0 Config
 
 
 
@@ -146,7 +145,7 @@ int main(void)
     GPIO_PORTE_AFSEL_R |= 4; /* enable alternate function */
     GPIO_PORTE_DEN_R &= ~4; /* disable digital function */
     GPIO_PORTE_AMSEL_R |= 4; /* enable analog function */
-    /* initialize ADC0 */
+    /* initialize ADC1 */
     ADC1_ACTSS_R &= ~8; /* disable SS3 during configuration */
     ADC1_EMUX_R &= ~0xF000; /* software trigger conversion */
     ADC1_SSMUX3_R = 1; /* get input from channel 1 */
@@ -160,9 +159,10 @@ int main(void)
         ; /* wait for conversion complete */
     voltage = ADC1_SSFIFO3_R; /* read conversion result */
     ADC1_ISC_R = 8; /* clear completion flag */
-
+    
+    //Calculate initial require_rpm
     require_rpm = 2400*(voltage/4095);
-
+    //Initial error percent
     error_percent = abs(100 * ((require_rpm - rpm) / require_rpm));
 
     //Timer Config
@@ -172,11 +172,14 @@ int main(void)
     ui32Period = SysCtlClockGet() / 0.5;
     //Set Timer_0A to the above frequency.
     TimerLoadSet(TIMER0_BASE, TIMER_A, ui32Period - 1);
+    
+    //Read ADC0
     ADC0_PSSI_R |= 8; /* start a conversion sequence 3 */
     while ((ADC0_RIS_R & 0x08) == 0)
         ; /* wait for conversion complete */
     result = ADC0_SSFIFO3_R; /* read conversion result */
     ADC0_ISC_R = 8; /* clear completion flag */
+    
     if (result > 2000)
     {
         initial_signal_value = 0;
@@ -190,7 +193,7 @@ int main(void)
 
     while (1)
     {
-
+        //Reset RPM value if the fan stop.
         if (ui8Adjust >= 750 && rpm < 300)
         {
             rpm = 0;
@@ -218,6 +221,7 @@ int main(void)
         voltage_sum = voltage_sum + ADC1_SSFIFO3_R; /* read conversion result */
         ADC1_ISC_R = 8; /* clear completion flag */
         voltage_count++;
+        //Find average for voltage input
         if (voltage_count == 50)
         {
             voltage = voltage_sum/50;
@@ -225,10 +229,6 @@ int main(void)
             voltage_sum = 0;
             voltage_count = 0;
         }
-
-
-
-
 
         if (result > 2000)
         {
@@ -238,7 +238,7 @@ int main(void)
         {
             comparision_sensor_value = 1;
         }
-
+        //Edge detected
         if (comparision_sensor_value != initial_signal_value)
         {
             if (count == 0)
@@ -253,7 +253,7 @@ int main(void)
                 count = 0;
                 period = (-final_timer_value + initial_timer_value);
                 rpm = round(60 / (period / SysCtlClockGet()));
-                //************Take Average of 3 Samples********************************************
+                //Calculate average RPM
                 countAvg++;
                 rpmSum = rpmSum + rpm;
                 if (countAvg == 3)
@@ -263,9 +263,7 @@ int main(void)
                     rounded = rpmAvg;
                     rpmSum = 0;
                 }
-                //************Take Average of 3 Samples********************************************
-                //************PROPORTIONAL SPEED CONTROL********************************************
-
+                //Set ui8adjust
                 error = ((require_rpm - rpmAvg) / 2400) * 1000;
                 error *= kp;
                 ui8Adjust = ui8Adjust - error;
@@ -281,13 +279,12 @@ int main(void)
                 {
                     ui8Adjust = 1000;
                 }
-
                 PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0,ui8Adjust * ui32Load / 1000);
-
-                //************PROPORTIONAL SPEED CONTROL********************************************
+                //Reset timer
                 TimerLoadSet(TIMER0_BASE, TIMER_A, ui32Period - 1);
             }
         }
+        //Save value for next loop
         if (result > 2000)
         {
             initial_signal_value = 0;
@@ -296,16 +293,17 @@ int main(void)
         {
             initial_signal_value = 1;
         }
-
+        //Percent error duh
         error_percent = abs(100 * ((require_rpm - rpm) / require_rpm));
+        //Percent duty circle
         duty = 100 - 100 * ((ui8Adjust * ui32Load / 1000) / (ui32Load));
+        //Break RPM into digits
         z = rounded % 10;
         yz = rounded % 100;
         xyz = rounded % 1000;
         y = (yz - z) / 10;
         x = (xyz - yz) / 100;
         w = (rounded - xyz) / 1000;
-        SysCtlDelay(5000);
 
         if (w > 0)
         {
@@ -389,7 +387,7 @@ void first(int Input) //PD2,PB3,PE4,PE5
     }
 }
 
-void second(int Input) //PE1, PE2, PB4, PA5
+void second(int Input) //PE1, PC4, PB4, PA5
 {
     switch (Input)
     {
